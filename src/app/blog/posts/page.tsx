b428d33cd6f20e221879ef83185b3182fa48c6e2
Eugene Zhuravel <ezhuravel@gmail.com>
2025-02-23 15:39:23 -0500

    added  test sql connection

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,15 +1,19 @@
+'use server'
 import { posts } from "@/app/lib/placeholder-data"
 import Posts from "@/app/ui/components/posts/Post"
+import { connectToDB } from "@/app/lib/data"
 
-export default function Page(){
+export default async function Page(){
+    const client = await connectToDB();
 
     return (
         <>
+        {client && <p className="text-green-500">Connected to Database</p>}
         <h2>Posts</h2>
 
         {posts.map((post) => (
             <Posts 
-            key={post.id}
+             key={post.id}
             {...post}/>            
         ))}
         </>
---
+++ src/app/lib/data.ts
@@ -0,0 +1,17 @@
+import {createClient} from "@vercel/postgres";
+
+export async function connectToDB(){
+    const client = createClient();
+    await client.connect();
+
+    try{
+        if(client){
+            console.log('Connected to Database')
+            return client;
+        }
+
+    }catch(error){
+        console.error("Error connecting to database", error);
+    }
+
+}
